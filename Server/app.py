37fae5d6032a575a54ec7e2ccd06257405e09d17
Mayur Chandanshive <your-email@gmail.com>
2026-09-21 14:47:26 +0530

    Add App.py

--- Server/app.py
+++ Server/app.py
@@ -17,7 +17,10 @@ app = FastAPI(title="HRMS API")
 # Allow React frontend to communicate with FastAPI
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://your-vercel-app.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
